f5427c470a1e674253fca11a50213bc88baee8b7
Janglee <53804912+votatntai@users.noreply.github.com>
2024-01-17 19:41:56 +0700

    Update APIUrl.dart

--- lib/providers/APIUrl.dart
+++ lib/providers/APIUrl.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 
 class APIUrl extends ChangeNotifier {
 
-  static const baseURL = 'https://6ea9-115-74-17-47.ngrok-free.app';
+  static const baseURL = 'https://7339-171-250-177-41.ngrok-free.app';
 
   static getUrl() {
     return baseURL;
